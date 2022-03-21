abed480590d6fe5fa613db6dd6c21faeae368620
Ahsan Bilal <ahsan.bilal.712@gmail.com>
2022-03-21 23:38:04 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React, {Fragment} from "react";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import Login from './Layout/Login';
-// import './App.css';
-export default function App() {
-  return(
-    <Provider store={store}>
-      <Router>
-      <Fragment>
-        <Routes>
-          <Route path="/" element={<Login/>}/> 
-        </Routes>
-      </Fragment>
-    </Router>
-  </Provider>
-    // <LoginSignUp/> 
-    // // <HomePage/> 
-  )
+
+import './App.css';
+import { Login } from './Layout/Login';
+
+function App() {
+  return (
+    <div className="App">
+        <Login/>
+    </div>
+  );
 }
 
+export default App;
